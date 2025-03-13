83ed35966993a1ffae4d05f30620e0e0ba216740
john48k⌖ <johncanal300@gmail.com>
2025-03-13 21:52:50 +0800

    07-Math Object

--- 06-Counter Program/index.js
+++ 06-Counter Program/index.js
@@ -1,25 +1,27 @@
 // Counter button
 
-
+// Get references to the HTML elements with the specified ids
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const countLabel = document.getElementById("countLabel");
 
+// Initialize a variable to keep track of the count
 let count = 0;
 
+// Add an event listener to the increase button that increments the count and updates the label
 increaseBtn.onclick = function () {
     count++;
     countLabel.textContent = count;
 }
 
-
+// Add an event listener to the decrease button that decrements the count and updates the label
 decreaseBtn.onclick = function () {
     count--;
     countLabel.textContent = count;
 }
 
-
+// Add an event listener to the reset button that resets the count to 0 and updates the label
 resetBtn.onclick = function () {
     count = 0;
     countLabel.textContent = count;
---
+++ 07-Math Object/index.js
@@ -0,0 +1,19 @@
+// Math =  built in object that provides  a collection of properties and methods for mathematical constants and functions.
+
+
+
+let x = 3.21;
+let y = 2;
+let z;
+
+// this rounds up the number 
+z = Math.round(x);
+
+
+// this rounds down the number
+z = Math.floor(x);
+
+z = Math.ceil(x);
+
+
+console.log(z);
