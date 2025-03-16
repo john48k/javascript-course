a795185f18a20f4e47e6955d2bb475cc23ee0f17
john48k⌖ <johncanal300@gmail.com>
2025-03-16 20:59:21 +0800

    10-Checked Property

---
+++ 10-Checked Properties/index.js
@@ -0,0 +1,35 @@
+// the checked properties determines which one gets selected 
+
+
+const myCheckBox = document.getElementById('myCheckBox');
+const visaBtn = document.getElementById('visaBtn');
+const masterCardBtn = document.getElementById('masterCardBtn');
+const payPalBtn = document.getElementById('payPalBtn');
+const mySubmit = document.getElementById('mySubmit');
+const subResult = document.getElementById('subResult');
+const paymentResult = document.getElementById('paymentResult');
+
+
+mySubmit.onclick   = function() {
+
+    if(myCheckBox.checked) {
+        subResult.textContent = `You have subscribed to our newsletter`;
+    }
+    else {
+        subResult.textContent = `You are not subscribed to our newsletter`;
+ 
+    }
+    if(visaBtn.checked){
+        paymentResult.textContent = `You have are paying with Visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You have are paying with MasterCard`;
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You have are paying with PayPal`;  
+    }
+    else {
+        paymentResult.textContent = `You have not selected a payment method`;
+    }
+
+}
