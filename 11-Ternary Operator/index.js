809904b89873acb80826e17e5901a1e9de679145
john48k ⌖ <johncanal300@gmail.com>
2025-03-18 10:29:20 +0800

    Ternary Operator

--- 11-Ternary Operator/index.js
+++ 11-Ternary Operator/index.js
@@ -1,2 +1,6 @@
-// ternary operator ? the question mark is like a shortcut for if else statements 
+// ternary operator ? the question mark is like a shortcut for if else statements
 // condition ? code if true : code if false
+
+let age = 21;
+
+let message = age >= 18 ? "Your an adult" : "Your below the age of 18";
