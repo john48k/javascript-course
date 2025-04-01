cd2d8a8e285e5f311268cd42767698b6edff86a6
john48k⌖ <johncanal300@gmail.com>
2025-04-01 15:18:41 +0800

    17-Strict Equality

---
+++ 17-Strict Equality/index.js
@@ -0,0 +1,15 @@
+// the guideline
+
+// = assignment operator
+// == comparison operator (compares values if they are equal)
+// === strict equality operator (this compares the values and the data types as well if they are all equal)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14;
+
+if (PI == "3.14") {
+  console.log("That is pie");
+} else {
+  console.log("That is not pie");
+}
